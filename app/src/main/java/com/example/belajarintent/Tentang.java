87223bcb805fcd1946e940e2ee68dcbffbabafe6
santafriskamanurung <santamanurung80@gmail.com>
2022-09-22 10:56:50 +0700

    pembaruan

---
+++ app/src/main/java/com/example/belajarintent/Tentang.java
@@ -0,0 +1,25 @@
+package com.example.belajarintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Tentang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+
+        Button kembali = findViewById(R.id.btnkembali);
+
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
