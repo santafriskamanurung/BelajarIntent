3957a561ca7ddc247597cf56398d6fa9b34641db
santafriskamanurung <santamanurung80@gmail.com>
2022-10-12 23:00:07 +0700

    belajar intent

--- app/src/main/java/com/example/belajarintent/Tentang.java
+++ app/src/main/java/com/example/belajarintent/Tentang.java
@@ -11,7 +11,7 @@ public class Tentang extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tentang);
+        setContentView(R.layout.activity_tentang2);
 
         Button kembali = findViewById(R.id.btnkembali);
 
